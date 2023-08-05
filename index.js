90ea5a6bfb561d0aea06bbd251e9adf3fa4084e7
Daniel Araujo Leal <daniellealuau@gmail.com>
2023-08-05 10:37:04 -0300

    conexao com bd feita

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const {Person} = require("./person");
+const dotenv = require('dotenv');
+const connectToDatabase = require('./src/connect');
 
-require('./modules/http');
+dotenv.config();
+
+connectToDatabase();
+
+
+//require('./modules/http');
+require('./modules/express')
+
+require
+
 
 const person = new Person('daniel');
 
---
+++ modules/express.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const UserModel = require('../src/models/user.model')
+const app = express();
+
+app.use(express.json());
+
+app.get('/home', (req, res) => {
+    res.setHeader("content-type", "text/html");
+    res.status(200).send('<h1> hello world </h1>');
+});
+
+
+app.get('/users', async (req, res) => {
+    try{
+        const users = await UserModel.find({});
+        res.status(200).json(users);
+    }catch{
+        return res.status(500).send(error.message);
+    }
+})
+app.post('/users', async (req, res) => {
+    try{
+        const user = await UserModel.create(req.body)
+        res.status(201).json(user);
+    }catch(error){
+        res.status(500).send(error.message);
+    }
+})
+
+const port = 8080;
+
+app.listen(port, () => console.log(`rodando com express na porta ${port}`));
